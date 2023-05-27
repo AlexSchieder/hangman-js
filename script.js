ed29bd7c25cb22a94f3370bfe9e2847548e92c6f
AlexSchieder <alex.schieder@posteo.net>
2023-05-27 23:06:24 +0200

    Add getWord + checkLetter func

--- script.js
+++ script.js
@@ -4,28 +4,19 @@ ToDo:
 	- categories / word length adjastable ?
 - Guess letters: 
 	- input: key is pressed + virtual keyboard
+	- check if letter has been tried + display all submitted letters
 	- check if word incluedes letter and where
 - lives: 
 	- display lives and update them
 	- visual hangman ?
 */
 const wordBox = document.getElementById('word');
+const submittedLettersList = document.getElementById('submittedLetters')
+const livesBox = document.getElementById('lives')
+let lives = 10;
+let occurrence = [];
+const word = localStorage.getItem('word' || '');
 
-function getWord () {
-	let word;
-	fetch(`https://www.wordgamedb.com/api/v1/words/random`)
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error(`HTTP error: ${response.status}`);
-			}
-			return response.json();
-		})
-			.then((json) => {
-				console.log(json.word)
-				renderWord(json.word)
-			})
-			.catch((err) => console.error(`Fetch problem: ${err.message}`));
-}
 
 function renderWord(word) {
 	for (let i = 0; i < word.length; i++) {
@@ -33,5 +24,53 @@ function renderWord(word) {
 	}
 }
 
-getWord()
+function wrongLetter(letter) {
+	console.log(`there is no ${letter} in this word`);
+}
 
+
+function checkLetter(letter, word) {
+	// check if letter exists in word
+	if (word.indexOf(letter) !== -1) {
+		// check where letter exists in word
+		let position = word.indexOf(letter)
+		while(position !== -1) {
+			occurrence.push(word.indexOf(letter, position))
+			position = word.indexOf(letter, position +1)
+		}
+		console.log(occurrence)
+		//return occurrence
+		renderLetter(letter, occurrence)
+	} else {
+		wrongLetter(letter)
+	}
+}
+
+function renderLetter(letter, occurrence) {
+	let letters = document.querySelectorAll('.letter')
+	console.log(letters)
+	for (let i = 0; i < occurrence.length; i++) {
+		let j = occurrence[i]
+		letters[j].innerHTML = `<span>${letter}</span>`
+	}
+}
+
+
+async function getWord() {
+	const response = await fetch(`https://www.wordgamedb.com/api/v1/words/random`)
+	if (!response.ok) {
+		const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+	}
+	const newWord = await response.json()
+	return newWord;
+}
+
+getWord()
+	.then(newWord => {
+		localStorage.setItem('word', newWord.word)
+		console.log(word)
+		renderWord(word)
+		checkLetter('e', word)
+	})
+	.catch(error => error.message)
